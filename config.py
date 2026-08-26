e71d5478d37b4d2fe64371e8a532bdb97d16a8c6
Jeiner Ramirez <jeinerramirez1910@gmail.com>
2026-08-26 16:52:58 -0500

    Nuevos cambios

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     
-    DATABASE_URL = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
