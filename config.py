f9fb757f5f276cfb5c62e8dfceb9996c6cc6428f
Jeiner Ramirez <jeinerramirez1910@gmail.com>
2026-08-26 16:22:34 -0500

    Nuevos cambios

--- config.py
+++ config.py
@@ -5,10 +5,8 @@ load_dotenv()
 
 class Config:
     
-    SQLALCHEMY_DATABASE_URI = os.getenv = (
-        "mysql://avnadmin:AVNS_NrYJtDmKn5ZuNZN_vZp@asistencia-jeinerramirez1910-5925.k.aivencloud.com:20970/defaultdb?ssl_mode=REQUIRED"
-    )
+    DATABASE_URL = os.getenv('DATABASE_URL')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    SECRET_KEY = ("UcHvyj7At9KlYjDsJVlyV3LXmZ1pEDO03WpDyNwFCC9Li2mBVGpWvss3Bu7_3OEKbrZCH7MyxttWHZHNzE2Yzg")
+    SECRET_KEY = os.getenv('SECRET_KEY')
